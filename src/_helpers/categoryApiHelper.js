6c568a4facc6a353e9b78955e23cc20b86372742
Konstantin Shilkov <konst.shilkov@gmail.com>
2024-07-10 15:05:56 +0200

    csrf impl in categories

--- src/_helpers/categoryApiHelper.js
+++ src/_helpers/categoryApiHelper.js
@@ -1,3 +1,4 @@
+import { getCsrfToken } from "./authApiHelpers";
 
 const REVALIDATE = 0;
 
@@ -11,7 +12,7 @@ export const getAll = async (pageNo, listSize, isSeance) => {
     if (res.ok) {
       const body = await res.json();
       console.log(body);
-      return body
+      return body;
     }
   } catch (error) {
     console.error("Error fetching categories:", error.message);
@@ -20,18 +21,24 @@ export const getAll = async (pageNo, listSize, isSeance) => {
 
 // find all categories as one list
 export async function getAllAsList() {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/categories?isSeance=true`, {
-    next: { revalidate: REVALIDATE },
-  }).catch((e) => {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_API_BASE_URL}/categories?isSeance=true`,
+    {
+      next: { revalidate: REVALIDATE },
+    }
+  ).catch((e) => {
     throw new Error("Failed to fetch data: " + e.message);
   });
   return res.json();
 }
 
 export async function getOneById(id) {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/categories/${id}`, {
-    next: { revalidate: REVALIDATE },
-  }).catch((e) => {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_API_BASE_URL}/categories/${id}`,
+    {
+      next: { revalidate: REVALIDATE },
+    }
+  ).catch((e) => {
     throw new Error("Failed to fetch data: " + e.message);
   });
   return res.json();
@@ -39,20 +46,32 @@ export async function getOneById(id) {
 
 // find all subcategories
 export async function getAllBySupercategory(id) {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/categories/${id}/subcategories`, {
-    next: { revalidate: REVALIDATE },
-  }).catch((e) => {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_API_BASE_URL}/categories/${id}/subcategories`,
+    {
+      next: { revalidate: REVALIDATE },
+    }
+  ).catch((e) => {
     throw new Error("Failed to fetch data: " + e.message);
   });
   return res.json();
 }
 
 export async function createOne(body) {
+  const csrfToken = await getCsrfToken();
+
   console.log(body);
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/categories`, {
-    method: "POST",
-    body: body,
-  }).catch((e) => {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_API_BASE_URL}/categories`,
+    {
+      method: "POST",
+      body: body,
+      headers: {
+        "X-XSRF-TOKEN": csrfToken, // add CSRF token to headers
+      },
+      credentials: "include",
+    }
+  ).catch((e) => {
     throw new Error("Failed to create data: " + e.message);
   });
   if (!res.ok) {
@@ -63,11 +82,20 @@ export async function createOne(body) {
 }
 
 export async function updateOneById(id, body) {
+  const csrfToken = await getCsrfToken();
+
   console.log(body.get("imageFile"));
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/categories/${id}`, {
-    method: "PUT",
-    body: body,
-  }).catch((e) => {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_API_BASE_URL}/categories/${id}`,
+    {
+      method: "PUT",
+      body: body,
+      headers: {
+        "X-XSRF-TOKEN": csrfToken, // add CSRF token to headers
+      },
+      credentials: "include",
+    }
+  ).catch((e) => {
     throw new Error("Failed to update data: " + e.message);
   });
   if (!res.ok) {
@@ -78,9 +106,19 @@ export async function updateOneById(id, body) {
 }
 
 export async function deleteOneById(id) {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/categories/${id}`, {
-    method: "DELETE",
-  }).catch((e) => {
+  const csrfToken = await getCsrfToken();
+
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_API_BASE_URL}/categories/${id}`,
+    {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        "X-XSRF-TOKEN": csrfToken, // add CSRF token to headers
+      },
+      credentials: "include",
+    }
+  ).catch((e) => {
     throw new Error("Failed to delete data: " + e.message);
   });
   return res.status;
@@ -88,28 +126,42 @@ export async function deleteOneById(id) {
 
 // category media management
 export async function createOneMedia(data) {
+  const csrfToken = await getCsrfToken();
+
   console.log(data);
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/categories/media`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(data),
-  }).catch((e) => {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_API_BASE_URL}/categories/media`,
+    {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json",
+        "X-XSRF-TOKEN": csrfToken, // add CSRF token to headers
+      },
+      credentials: "include",
+    }
+  ).catch((e) => {
     throw new Error("Failed to create data: " + e.message);
   });
   return res.json();
 }
 
 export async function deleteOneMediaById(id) {
+  const csrfToken = await getCsrfToken();
+
   console.log(data);
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/categories/media/${id}`, {
-    method: "DELETE",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(data),
-  }).catch((e) => {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_API_BASE_URL}/categories/media/${id}`,
+    {
+      method: "DELETE",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json",
+        "X-XSRF-TOKEN": csrfToken, // add CSRF token to headers
+      },
+      credentials: "include",
+    }
+  ).catch((e) => {
     throw new Error("Failed to create data: " + e.message);
   });
   return res.json();
@@ -117,7 +169,9 @@ export async function deleteOneMediaById(id) {
 
 export const getOneMediaFile = async (id, setImageSrc) => {
   try {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/categories/${id}/image`);
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_API_BASE_URL}/categories/${id}/image`
+    );
     if (response.ok) {
       const blob = await response.blob();
       const objectURL = URL.createObjectURL(blob);
